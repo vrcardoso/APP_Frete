265f909667f71aeb539f1fef9bffa90dfd72b9d0
vrcardoso <viti26.ramos@gmail.com>
2023-08-02 16:04:58 -0300

    Passport
    Implementacao do passport com jwt

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -4,48 +4,59 @@ module.exports = app => {
     app.post('/validateToken', app.api.auth.validateToken)
 
     app.route('/users')
+        .all(app.config.passport.authenticate())
         .post(app.api.user.save)
         .get(app.api.user.get)
 
     app.route('/users/:id')
+        .all(app.config.passport.authenticate())
         .put(app.api.user.save)
 
     app.route('/produtos')
+        .all(app.config.passport.authenticate())
         .post(app.api.produto.save)
         .get(app.api.produto.get)
 
     app.route('/produtos/:id')
+        .all(app.config.passport.authenticate())
         .get(app.api.produto.getById)
         .put(app.api.produto.save)
         .delete(app.api.produto.remove)
 
     app.route('/veiculos')
+        .all(app.config.passport.authenticate())    
         .post(app.api.veiculo.save)
         .get(app.api.veiculo.get)
 
     app.route('/veiculos/:id')
+        .all(app.config.passport.authenticate())
         .get(app.api.veiculo.getById)
         .put(app.api.veiculo.save)
         .delete(app.api.veiculo.remove)
 
     app.route('/fretes')
+        .all(app.config.passport.authenticate())
         .post(app.api.frete.save)
         .get(app.api.frete.get)
 
     app.route('/fretes/:id')
+        .all(app.config.passport.authenticate())
         .get(app.api.frete.getById)
         .put(app.api.frete.save)
         .delete(app.api.frete.remove)
 
     app.route('/historicos')
+        .all(app.config.passport.authenticate())
         .post(app.api.historico.save)
         .get(app.api.historico.get)
 
     app.route('/historicos/:id')
+        .all(app.config.passport.authenticate())
         .get(app.api.historico.getById)
         .put(app.api.historico.save)
         .delete(app.api.historico.remove)
 
     app.route('/users/:id/historico')
+        .all(app.config.passport.authenticate())
         .get(app.api.historico.getByUser)
 }
